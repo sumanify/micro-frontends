ed0ed200261dd3a8a809f075e73f27a61e0123df
Suman Kharel <sumanify@gmail.com>
2023-05-15 15:16:32 +0300

    Asynchronous loading

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
-import Header from "home/Header";
+const Header = React.lazy(() => import("home/Header"));
 import Footer from "home/Footer";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <Header />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Header />
+    </Suspense>
     <main class="container mx-auto px-4 py-8">
       <h2 class="text-2xl font-bold mb-4">Welcome to my website!</h2>
       <p class="mb-4"> Page content </p>
